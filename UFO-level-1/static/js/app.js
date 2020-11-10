1e60cd6601fdcb32fa78d9cdf0cc4b8d4d33cc4e
coreymcculley <corey.mcculley@gmail.com>
2020-11-10 15:22:49 -0600

    Created function to filter data and rewrite the table using the date

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,29 +2,62 @@
 var tableData = data;
 console.log(tableData);
 // YOUR CODE HERE!
-// Get a reference to the table body
+
+// Select the table body so we can input the UFO siting data
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
-console.log(data);
+//Loop Through `data` and log each UFO report
+function renderTable(data1){   
+    
+    data1.forEach(ufoData => {
+    //console.log(ufoData);
 
+    // Append a new row for each row of UFO data
+    var row = tbody.append("tr");
 
-// Step 1: Loop Through `data` and log each weather report object
-data.forEach(ufoData => {
-console.log(ufoData);
+    // Log each UFO report value and add data to the table
+    Object.entries(ufoData).forEach(([key, value]) => {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+};    
+//HTML now has table for filtering
+renderTable(tableData);
 
-  // Step 2:  Use d3 to append one table row `tr` for each weather report object
-  var row = tbody.append("tr");
+//Get input from the text box, filter for the matching dates and output table of results. 
+//Also include an error popup if searched data does not match the anything in the provided data.
+var button = d3.select("#filter-btn");
+//filter data based on user input
+button.on("click", function() {
 
-  // Step 3:  Use `Object.entries` to log each weather report value
-  Object.entries(ufoData).forEach(([key, value]) => {
-    console.log(key, value);
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    //console.log(inputValue);
 
-    // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-    var cell = row.append("td");
-
-    // Step 5: Use d3 to update each cell's text with
-    // weather report values (weekday, date, high, low)
-    cell.text(value);
-  });
+    var filteredData = tableData.filter(ufoData => ufoData.datetime === inputValue);
+    //console.log(filteredData);
+    if (Object.keys(filteredData).length === 0){
+        errorPopup();
+    }
+    else {
+        clearTable();
+        renderTable(filteredData);
+    }
 });
+
+function clearTable(){
+    var tableHeaderRowCount = 1;
+    var table = document.getElementById('ufo-table');
+    var rowCount = table.rows.length;
+    for (var i = tableHeaderRowCount; i < rowCount; i++) {
+        table.deleteRow(tableHeaderRowCount);
+    }
+};
+
+function errorPopup() {
+    alert("Date does not match any UFO report");
+};
